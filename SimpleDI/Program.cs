f4ff2665b2b3eb0195d274a2fb4a007daeee4235
SergiroZ <zujonok@gmail.com>
2019-04-25 00:42:16 +0300

    added helpers

--- SimpleDI/Program.cs
+++ SimpleDI/Program.cs
@@ -69,15 +69,24 @@ namespace SimpleDI
     {
         private static readonly Dictionary<Type, Type> RegisteredObjects =
             new Dictionary<Type, Type>();
-
-        public static dynamic Resolve<TKey>()
-        {
-            return Activator.CreateInstance(RegisteredObjects[typeof(TKey)]);
-        }
-
+        /// <summary>
+        /// Регистрация объекта в контейнере 
+        /// </summary>
+        /// <typeparam name="TKey">Тип абстракции</typeparam>
+        /// <typeparam name="TConcrete">Тип объекта</typeparam>
         public static void Register<TKey, TConcrete>() where TConcrete : TKey
         {
             RegisteredObjects[typeof(TKey)] = typeof(TConcrete);
+        }
+
+        /// <summary>
+        /// Получение объекта из контейнера
+        /// </summary>
+        /// <typeparam name="TKey">Тип абстракции</typeparam>
+        /// <returns>Объект</returns>
+        public static dynamic Resolve<TKey>()
+        {
+            return Activator.CreateInstance(RegisteredObjects[typeof(TKey)]);
         }
     }
 
